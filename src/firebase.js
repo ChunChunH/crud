815936ba128ce5e3cfb3a03a4d974caeb11ed8ca
ChunChunH <agussolheredia@gmail.com>
2021-05-13 10:40:52 -0300

    persisted store completed

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY || "",
-    authDomain: "product-list-auth.firebaseapp.com",
-    projectId: "product-list-auth",
-    storageBucket: "product-list-auth.appspot.com",
-    messagingSenderId: "599520234120",
-    appId: "1:599520234120:web:97a97e04331834c8c76d3d",
-    measurementId: "G-H9WKECCH9B"
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN || "",
+    projectId: process.env.REACT_APP_PROJECT_ID || "",
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET || "",
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || "",
+    appId: process.env.REACT_APP_APP_ID || "",
+    measurementId: proccess.env.REACT_APP_MEASUREMENT_ID || "",
   };
 
 
